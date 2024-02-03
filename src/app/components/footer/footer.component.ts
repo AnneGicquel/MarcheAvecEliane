64538e151429107870b5fdc56092e1b1a4d88a02
AnneGicquel <annelaedere@gmail.com>
2024-02-04 00:21:04 +0100

    Add homePage HTML, CSS, and make it responsive

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SocialNetworkComponent } from "../social-network/social-network.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
-    imports: [SocialNetworkComponent]
+    imports: [RouterModule, SocialNetworkComponent]
 })
 export class FooterComponent {
 
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
