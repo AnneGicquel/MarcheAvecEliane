c08fb8f52b68ade89db7ef3233e94998cad55957
AnneGicquel <annelaedere@gmail.com>
2024-02-09 22:38:35 +0100

    Added _reset password_ from accountSettingsPage

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-account-settings',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './account-settings.component.html',
   styleUrl: './account-settings.component.css',
 })
@@ -13,12 +14,12 @@ export class AccountSettingsComponent implements OnInit {
   userForm!: FormGroup;
   validationError: string[] = [];
 
-
   constructor(private titleService: Title, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('MaÉ ♡ | Paramètres du compte');
 
+    // FORM
     this.userForm = this.formBuilder.group({
       firstName: [null, [Validators.required]],
       lastName: [null, [Validators.required]],
@@ -34,7 +35,7 @@ export class AccountSettingsComponent implements OnInit {
     });
   }
 
-  // check if valid to do...
+  // IS VALID ? => SUBMIT
   submitModifications() {
     console.log(this.userForm.value)
     this.validationError = [];
@@ -48,6 +49,22 @@ export class AccountSettingsComponent implements OnInit {
     } else {
       // Si valide, enregistre en base de données et lacent une modale qui confirme les modifif enregistrées
     }
+  }
+
+  // TOGGLE EYE OPEN/CLOSE
+  isPasswordVisible = {
+    old: false,
+    new: false,
+    confirm: false
+  };
+
+  togglePasswordVisibility(field: 'old' | 'new' | 'confirm') {
+    this.isPasswordVisible[field] = !this.isPasswordVisible[field];
+  }
+
+  // EASY ACCESS to the password form control
+  get passwordFormField() {
+    return this.userForm.get('newPassword');
   }
 }
 
