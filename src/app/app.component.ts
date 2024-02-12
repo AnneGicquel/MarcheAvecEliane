6431fa9b1a01199b846209f4a9e443459c707fdd
AnneGicquel <annelaedere@gmail.com>
2024-02-12 12:47:28 +0100

    test: bootstrap installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { SocialNetworkComponent } from "./components/social-network/social-network.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, SocialNetworkComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, SocialNetworkComponent, NgbModule]
 })
 export class AppComponent {
   title = 'Marche Avec Éliane';
